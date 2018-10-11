77b5dcd85b6ff99dc07d71b8ae8e57e6ef8afa95
ddtobiasz <dan.tobias@appian.com>
2018-10-11 10:00:04 -0400

    Committed by testAppianImports

---
+++ ddl_files/qckapp_sdwa_main.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `qckapp_sdwa_main` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `folderid` int(11) DEFAULT NULL,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createdon` datetime DEFAULT NULL,
+  `updatedby` varchar(255) DEFAULT NULL,
+  `updatedon` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `qckapp_sdwa_main`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
